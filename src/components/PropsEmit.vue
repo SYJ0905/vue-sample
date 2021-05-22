2058c5c82a3f3af1280e696511ed3c63e3f958e0
DESKTOP-HBH8EAQ\Cloud <BetaPhoenixSNSD@gmail.com>
2021-05-22 15:12:38 +0800

    feat:PropsEmit元件

---
+++ src/components/PropsEmit.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+    <select v-model="propsCity">
+      <option
+        v-for="item in cityList"
+        :key="item.id"
+        :value="item.name"
+      >
+        {{ item.name }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<!--
+  父元素寫法
+  <template>
+    <PropsEmit
+      :city="city"
+      :city-list="cityList"
+      @handle_city="update_city"
+    />
+  </template>
+  <script>
+  import PropsEmit from '@/components/PropsEmit.vue';
+  export default {
+    components: {
+      PropsEmit,
+    },
+    data() {
+      return {
+        city: '台北市',
+        cityList: [
+          {
+            id: 1,
+            name: '台北市',
+          },
+          {
+            id: 2,
+            name: '新北市',
+          },
+        ],
+      };
+    },
+    methods: {
+      update_city(val) {
+        this.city = val;
+      },
+    },
+  };
+  </script>
+-->
+
+<script>
+export default {
+  props: {
+    city: {
+      type: String,
+      default() {
+        return '';
+      },
+    },
+    cityList: {
+      type: Array,
+      default() {
+        return [];
+      },
+    },
+  },
+  computed: {
+    propsCity: {
+      get() {
+        return this.city;
+      },
+      set(val) {
+        this.$emit('handle_city', val);
+      },
+    },
+  },
+};
+</script>
