d61dd68c96f6939c8ae59ecbdaa87c44592ad95f
Cloud <BetaPhoenixSNSD@gmail.com>
2021-09-05 02:59:04 +0800

    feat:讀取/寫入 Store Dada 方法

---
+++ src/components/SetStoreData.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <h1>讀取/寫入 Store Dada 方法:</h1>
+    <input
+      v-model.trim="data.name"
+      type="text"
+      placeholder="姓名"
+      @input="update"
+    >
+    <input
+      v-model.number="data.age"
+      type="number"
+      placeholder="年齡"
+      @input="update"
+    >
+    <div>
+      姓名: {{ storData.name }}
+      年齡: {{ storData.age }}
+    </div>
+  </div>
+</template>
+
+<script>
+import _ from 'lodash';
+import { mapMutations } from 'vuex';
+
+export default {
+  computed: {
+    data() {
+      return _.cloneDeep(this.$store.getters.data);
+    },
+    storData: {
+      get() {
+        return this.$store.getters.data;
+      },
+    },
+  },
+  methods: {
+    ...mapMutations([
+      'UPDATE_DATA',
+    ]),
+    update() {
+      this.UPDATE_DATA(this.data);
+    },
+  },
+};
+</script>
+
+<style>
+
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -8,13 +8,22 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   strict: true,
   state: {
+    data: {
+      name: 'Cloud',
+      age: 27,
+    },
   },
   mutations: {
+    UPDATE_DATA(state, payload) {
+      state.data = payload;
+    },
   },
   actions: {
   },
   getters: {
-
+    data(state) {
+      return state.data;
+    },
   },
   modules: {
     sample,
